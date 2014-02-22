271f5f3a5b48998955a49d5db840f87858a7925c
Daniel Aleksandersen <code@daniel.priv.no>
2014-02-22 22:43:01 +0100

    Use specific search engine by keyword

--- background.js
+++ background.js
@@ -1,30 +1,45 @@
-function searchURI ( encodedInput )
+// Requirements: General purpose, English, HTTPS
+var searchProviders = {
+    "duckduckgo" : "https://duckduckgo.com/?q=",
+    "ixquick" : "https://ixquick.com/do/search?q=",
+    "yahoo" : "https://search.yahoo.com/search?q=",
+    "bing" : "https://www.bing.com/search?q=",
+    "gigablast" : "https://www.gigablast.com/search?q=",
+    "google" : "https://www.google.no/search?q=",
+    "yandex" : "https://www.yandex.com/yandsearch?text="
+  },
+  searchProvidersKeys = Object.keys( searchProviders );
+
+function randSearch ( input )
 {
-  var searchProviders = [
-    // Requirements: General purpose, English, HTTPS
-    "duckduckgo" => "https://duckduckgo.com/?q=",
-    "https://ixquick.com/do/search?q=",
-    "https://search.yahoo.com/search?q=",
-    "https://www.bing.com/search?q=",
-    "https://www.gigablast.com/search?q=",
-    "https://www.google.no/search?q=",
-    "https://www.yandex.com/yandsearch?text="
-  ],
-  ranArrLenNum = Math.floor( Math.random() * searchProviders.length ),
-  provider = searchProviders[ ranArrLenNum ];
-  return provider + encodedInput
+  var input = input.trim(),
+    guessProviderKeyword = input.split( " " )[0];
+  // Search specific provider using keyword
+  if ( ( guessProviderKeyword in searchProviders ) && guessProviderKeyword.length < input.length + 2 )
+  {
+    var keywordStrippedInput = input.slice( guessProviderKeyword.length + 1, input.lenght ),
+      encodedInput = encodeURIComponent( keywordStrippedInput );
+    return searchProviders[ guessProviderKeyword ] + encodedInput;
+  }
+  // Search using a random provider
+  else
+  {
+    var randProviderKey = searchProvidersKeys[ Math.floor( Math.random() * searchProvidersKeys.length ) ],
+      providerURL = searchProviders[ randProviderKey ],
+      encodedInput = encodeURIComponent( input );
+    return providerURL + encodedInput;
+  }
 }
 
 function inputHandler( input )
 {
   if ( input )
   {
-    var encodedInput = encodeURIComponent( input );
     chrome.tabs.query(
       { 'currentWindow': true, 'active': true },
       function( tab )
       {
-        chrome.tabs.update( tab[0].id, { "url" : searchURI( encodedInput ) } );
+        chrome.tabs.update( tab[0].id, { "url" : randSearch( input ) } );
       }
     )
   }
